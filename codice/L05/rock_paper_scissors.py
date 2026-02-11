ae43c545cbeb5b452fe1b2e7a0ff38c2c88817ad
maboglia <mauro.bogliaccino@gmail.com>
2026-02-11 13:21:24 +0100

    giochini python: lancia dadi & rock, paper, scissors

---
+++ codice/L05/rock_paper_scissors.py
@@ -0,0 +1,21 @@
+""" giochiamo a sasso carta forbice """
+
+
+import random
+
+scelte = ["sasso", "carta", "forbice"]
+
+while True:
+    print(f"Gioca a {scelte}")
+    umano = input(f"Scegli tra: {scelte}. Premi q per uscire ")
+
+    if umano.lower() == 'q':
+        break
+
+    macchina = random.choice(scelte)
+    
+    print(f"Umano ha scelto {umano}")
+    print(f"Macchina ha scelto {macchina}")
+
+print("Game over")
+print("Grazie per aver giocato con noi!")
