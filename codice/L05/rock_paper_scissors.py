95eeac14b5c51cfab88179bf46545bdf02118c36
maboglia <mauro.bogliaccino@gmail.com>
2026-02-11 13:34:21 +0100

    rock papaer scissors

--- codice/L05/rock_paper_scissors.py
+++ codice/L05/rock_paper_scissors.py
@@ -13,9 +13,17 @@ while True:
         break
 
     macchina = random.choice(scelte)
-    
+
     print(f"Umano ha scelto {umano}")
     print(f"Macchina ha scelto {macchina}")
+
+    if umano == macchina:
+        print("Pareggio!")
+    elif (umano == "sasso" and macchina == "forbice") or (umano == "carta" and macchina == "sasso") or (umano == "forbice" and macchina == "carta"):
+        print("Ha vinto umano!")
+    else:
+        print("Hai vinto macchina!")
+
 
 print("Game over")
 print("Grazie per aver giocato con noi!")
